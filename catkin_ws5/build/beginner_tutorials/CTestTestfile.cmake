ed1401ef45780adb1fd61889b3300d9b3e06eb87
Lilian Lamb <57109743+Mysterium-sch@users.noreply.github.com>
2024-03-26 11:46:42 -0400

    upload pd controller

---
+++ catkin_ws5/build/beginner_tutorials/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/lixion/Desktop/school/CSCE574/catkin_ws5/src/beginner_tutorials
+# Build directory: /home/lixion/Desktop/school/CSCE574/catkin_ws5/build/beginner_tutorials
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ catkin_ws5/src/random_walk/src/random_walk.cpp
@@ -0,0 +1,175 @@
+/*
+ * Code modified by Peyton Chandarana
+ * for CSCE 574 Homework 1
+ * Spring 2024
+ */
+
+#include <cstdlib>  // Needed for rand()
+#include <ctime>    // Needed to seed random number generator with a time value
+
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "nav_msgs/Odometry.h"
+#include "geometry_msgs/Twist.h"
+#include <tf/transform_listener.h>
+#include <vector>
+
+class RandomWalk {
+ public:
+  // Construst a new RandomWalk object and hook up this ROS node
+  // to the simulated robot's velocity control and laser topics
+  RandomWalk(ros::NodeHandle& nh)
+      : rotateStartTime(ros::Time::now()),
+        rotateDuration(0.f) {
+    // Initialize random time generator
+    srand(time(NULL));
+
+    // Advertise a new publisher for the simulated robot's velocity command
+    // topic (the second argument indicates that if multiple command messages
+    // are in the queue to be sent, only the last command will be sent)
+    commandPub = nh.advertise<geometry_msgs::Twist>("cmd_vel", 1);
+
+    poseSub = nh.subscribe("base_pose_ground_truth", 1, \
+      &RandomWalk::poseCallback, this);
+
+    // Subscribe to the simulated robot's laser scan topic and tell ROS to call
+    // this->commandCallback() whenever a new message is published on that topic
+    laserSub = nh.subscribe("base_scan", 1, &RandomWalk::commandCallback, this);
+  };
+
+  // Send a velocity command
+  void move(double linearVelMPS, double angularVelRadPS) {
+    geometry_msgs::Twist
+        msg;  // The default constructor will set all commands to 0
+    msg.linear.x = linearVelMPS;
+    msg.angular.z = angularVelRadPS;
+    commandPub.publish(msg);
+  };
+
+  // Process the incoming laser scan message
+  void commandCallback(const sensor_msgs::LaserScan::ConstPtr& msg) {
+    float angle_mini = msg->angle_min;
+    float angle_max = msg->angle_max;
+    float angle_inc = msg->angle_increment;
+    
+    unsigned int minIndex =
+        ceil((MIN_SCAN_ANGLE_RAD - msg->angle_min) / msg->angle_increment);
+    unsigned int maxIndex =
+        ceil((MAX_SCAN_ANGLE_RAD - msg->angle_min) / msg->angle_increment);
+    unsigned int closestIndex = minIndex;
+    // Start at the minimum index
+    closestRange = msg->ranges[minIndex];
+    
+    int count = 0;
+    for(float i = angle_mini; i<(angle_max-angle_inc); i=i+angle_inc) {
+      if (msg->ranges[count] < closestRange) {
+        closestRange = msg->ranges[count];
+        closestAngle = i;
+      }
+      count++;
+    }
+  };
+
+  // Process incoming ground truth robot pose message
+  void poseCallback(const nav_msgs::Odometry::ConstPtr& msg) {
+    double roll, pitch;
+    x = -msg->pose.pose.position.y;
+    y = msg->pose.pose.position.x;
+    heading=tf::getYaw(msg->pose.pose.orientation)+0.5*3.14;
+  };
+  
+
+  float calculate_desired_heading(){
+
+  };
+  // Main FSM loop for ensuring that ROS messages are
+  // processed in a timely manner, and also for sending
+  // velocity controls to the simulated robot based on the FSM state
+  void spin() {
+    bool found = false;
+    bool start = true;
+    ros::Rate rate(10);  // Specify the FSM loop rate in Hz
+    while (ros::ok()) {  // Keep spinning loop until user presses Ctrl+C
+      /////////////////////// ANSWER CODE BEGIN ///////////////////
+      std::cout << "Closest range: " << closestRange << " At angle: " << closestAngle << " heading " <<  heading << "\n";
+      
+      // When the program starts up, go until find a wall then rotate to be perpendicular
+      if(start) {
+        if (closestRange < xd) {
+          found = true;
+          // Now that I have found a wall, I always want to be perpendicular to the wall at the closest range
+          if(!(closestAngle > -1.45 && closestAngle < -1.35)) {
+            move(0, ROTATE_SPEED_RADPS);
+          } else {
+            start = false;
+          }
+        } else if (!found) {
+          // Let's move forward untl we find the wall
+          move(FORWARD_SPEED_MPS, 0);
+        }
+      } else {
+        // error time
+        float error = closestRange - xd;
+        in_eror += error;
+        float dev_error = error - prev_error;
+        float turn = p * error + d * dev_error;
+        prev_error = error;
+        std::cout << "Turn rate from error: " << turn << "\n\n";
+        move(0.2, turn);
+      }
+      /////////////////////// ANSWER CODE END ///////////////////
+      ros::spinOnce();  // Need to call this function often to allow ROS to
+                        // process incoming messages
+      rate.sleep();  // Sleep for the rest of the cycle, to enforce the FSM loop
+                     // rate
+    }
+  };
+
+  // Tunable parameters
+  // TODO: tune parameters as you see fit
+  constexpr static double MIN_SCAN_ANGLE_RAD = -10.0 / 180 * M_PI;
+  constexpr static double MAX_SCAN_ANGLE_RAD = +10.0 / 180 * M_PI;
+  constexpr static float PROXIMITY_RANGE_M =
+      1.0;  // Should be smaller than  sensor_msgs::LaserScan::range_max
+  constexpr static double FORWARD_SPEED_MPS = 1.0;
+  constexpr static double BACKWARD_SPEED_MPS = -0.5;
+  constexpr static double ROTATE_SPEED_RADPS = M_PI / 10;
+
+ protected:
+  ros::Publisher commandPub;  // Publisher to the simulated robot's velocity command topic
+  ros::Subscriber laserSub;  // Subscriber to the simulated robot's laser scan topic
+  ros::Subscriber poseSub; // Subscriber to the current robot's ground truth pose topic
+
+
+  ros::Time rotateStartTime;     // Start time of the rotation
+  ros::Duration rotateDuration;  // Duration of the rotation
+  // Added for detecting stuck state:
+  ros::Time reverseStartTime;
+  ros::Duration reverseDuration;
+  unsigned int prevClosestIndex;
+  float prevClosestRange;
+  float closestRange = 5;
+  unsigned int direction = 0;
+  float closestAngle;
+
+  float p = 0.5;
+  float i = 0.01;
+  float d = 0.2;
+
+  float in_eror = 0;
+  float prev_error = 0;
+  float xd = 2;
+
+  double x; // in simulated Stage units, + = East/right
+  double y; // in simulated Stage units, + = North/up
+  double heading; // in radians, 0 = East (+x dir.), pi/2 = North (+y dir.)
+};
+
+int main(int argc, char** argv) {
+  ros::init(argc, argv,
+            "random_walk");  // Initiate new ROS node named "random_walk"
+  ros::NodeHandle n;
+  RandomWalk walker(n);  // Create new random walk object
+  walker.spin();         // Execute FSM loop
+  return 0;
+};
